4d5274c8828618ca1c866a6fe269abf132c76c26
marinahaid <marinaniksol@gmail.com>
2021-04-22 13:01:09 +0200

    truck2

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,7 @@
 "use strict";
-
-;
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-
-// Close the dropdown menu if the user clicks outside of it
 window.onclick = function(event) {
 if (!event.target.matches('.dropbtn')) {
 
@@ -19,3 +15,31 @@ if (!event.target.matches('.dropbtn')) {
   }
 }
 }
+
+jQuery(($) => {
+  $('.select').on('click', '.select__head', function () {
+      if ($(this).hasClass('open')) {
+          $(this).removeClass('open');
+          $(this).next().fadeOut();
+      } else {
+          $('.select__head').removeClass('open');
+          $('.select__list').fadeOut();
+          $(this).addClass('open');
+          $(this).next().fadeIn();
+      }
+  });
+
+  $('.select').on('click', '.select__item', function () {
+      $('.select__head').removeClass('open');
+      $(this).parent().fadeOut();
+      $(this).parent().prev().text($(this).text());
+      $(this).parent().prev().prev().val($(this).text());
+  });
+
+  $(document).click(function (e) {
+      if (!$(e.target).closest('.select').length) {
+          $('.select__head').removeClass('open');
+          $('.select__list').fadeOut();
+      }
+  });
+});
