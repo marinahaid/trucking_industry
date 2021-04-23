3e48b32d58c46da02ad92d1a06f898306cfda8c9
marinahaid <marinaniksol@gmail.com>
2021-04-23 12:29:32 +0200

    22

--- source/js/script.js
+++ source/js/script.js
@@ -55,8 +55,6 @@ if (!event.target.matches('.dropbtn2')) {
 }
 }
 
-
-
 function myFunction3() {
   document.getElementById("myDropdown3").classList.toggle("show");
 }
@@ -74,3 +72,55 @@ if (!event.target.matches('.dropbtn3')) {
   }
 }
 }
+
+
+function myFunction4() {
+  document.getElementById("myDropdown4").classList.toggle("show");
+}
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn4')) {
+
+    var dropdowns = document.getElementsByClassName("content4");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+  }
+
+  function myFunction5() {
+    document.getElementById("myDropdown5").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn5')) {
+
+      var dropdowns = document.getElementsByClassName("content5");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+    }
+
+    function myFunction6() {
+      document.getElementById("myDropdown6").classList.toggle("show");
+    }
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn6')) {
+
+        var dropdowns = document.getElementsByClassName("content6");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
+        }
+      }
+      }
